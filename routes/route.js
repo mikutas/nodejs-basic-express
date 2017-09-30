87f5c6658ddd810c8e39f75f618ef5265c62c141
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2017-09-30 18:39:17 +0900

    レクチャー19完了

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,4 @@
-var atricles = [];
+var articles = [];
 
 module.exports = function(express){
 
@@ -6,19 +6,28 @@ module.exports = function(express){
 
     router.route("/articles")
         .get(function(req, res){
-        res.json(articles);
-    }).post(function(req, res){
-        articles.push(req.body);
-        res.redirect("/articles");
-    });
+            res.json(articles);
+        }).post(function(req, res){
+            articles.push(req.body);
+            res.redirect("/articles");
+        });
 
     router.route("/articles/:id")
         .get(function(req, res){
 
-    }).put(function(req, res){
+        }).put(function(req, res){
 
-    }).delete("/articles/:id", function(req, res){
+        }).delete(function(req, res){
 
+        });
+
+    router.get("/new", function(req, res){
+        res.send('<form method="post" action="/articles">\
+            <input type="text" placeholder="タイトル" name="title">\
+            <input type="text" placeholder="投稿者" name="author">\
+            <textarea placeholder="内容" name="body"></textarea>\
+            <button type="submit">投稿する</button>\
+            </form>');
     });
 
     return router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var morgan = require("morgan");
 
 app.use(morgan("dev"));
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 var router = require("./routes/route")(express);
 app.use("/", router);
 
