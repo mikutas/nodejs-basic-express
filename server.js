c2c09a6c33b4d39ad2d1c9e8ce9280651bf1200f
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2017-09-30 18:17:16 +0900

    レクチャー16完了

--- server.js
+++ server.js
@@ -9,16 +9,27 @@ app.get("/hello/:name", function(req, res){
     res.send("Hello, " + req.params.name + "!");
 });
 
-app.get("/hello", function(req, res){
-    res.send("Hello, world!");
+var articles = [];
+
+app.get("/articles", function(req, res){
+    res.json(articles);
 });
 
-app.get("/goodbye", function(req, res){
-    res.send("Good-bye, world!");
+app.post("/articles", function(req, res){
+    articles.push(req.body);
+    res.redirect("/articles");
 });
 
-app.all("*", function(req, res){
-    res.sendStatus(404);
+app.get("/articles/:id", function(req, res){
+
+});
+
+app.put("/articles/:id", function(req, res){
+
+});
+
+app.delete("/articles/:id", function(req, res){
+
 });
 
 app.listen(3000, function(){
